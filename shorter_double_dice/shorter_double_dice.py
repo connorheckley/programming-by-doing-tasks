9f16fc68135a480173d117f5c8a8436464af39db
Connor Heckley <58855366+connorheckley@users.noreply.github.com>
2021-02-07 14:20:23 +0000

    Add files via upload

---
+++ shorter_double_dice/shorter_double_dice.py
@@ -0,0 +1,12 @@
+import random
+# NUMBER GUESSING WITH A COUNTER
+
+print("Her comes the dice")
+print()
+
+roll_1 = random.randint(1,6)
+print("roll one", roll_1)
+roll_2 = random.randint(1,6)
+print("roll two", roll_2)
+# The total is 8!
+print("The total is: ", roll_1 + roll_2)
